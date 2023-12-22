5fe1b035f91e26518deeb01701ff3ca7f4cc6dd7
Gustavo José <89671108+0gudu@users.noreply.github.com>
2023-12-22 02:22:40 -0300

    Add files via upload

---
+++ inserir.php
@@ -0,0 +1,36 @@
+<?php
+// Informações de conexão com o banco de dados
+$host = 'localhost';
+$dbname = 'bd';
+$user = 'root';
+$pass = '';
+
+// Nome da tabela
+$tabela = 'dados';
+
+try {
+    // Conectar ao banco de dados usando PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Caminho do arquivo CSV
+    $arquivo_csv = $_POST['caminho'];
+
+    // Montar a consulta SQL de inserção
+    $colunas = ['fds', 'urls', 'email','senha']; // Substitua pelos nomes reais das suas colunas
+    $sql = "LOAD DATA INFILE :arquivo
+            INTO TABLE $tabela
+            FIELDS TERMINATED BY ':'
+            LINES TERMINATED BY '\n'
+            (" . implode(', ', $colunas) . ")";
+
+    // Preparar e executar a consulta SQL
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':arquivo', $arquivo_csv, PDO::PARAM_STR);
+    $stmt->execute();
+
+    echo "Dados inseridos com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro ao inserir dados: " . $e->getMessage();
+}
+?>
