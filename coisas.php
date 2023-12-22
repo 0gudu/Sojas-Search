544c720fcd88cb5c9eb5db2983a97b47acaf8adb
Gustavo José <89671108+0gudu@users.noreply.github.com>
2023-12-22 00:52:34 -0300

    Add files via upload

--- coisas.php
+++ coisas.php
@@ -7,11 +7,11 @@
         $stmt = $pdo->prepare("SELECT * FROM dados WHERE urls LIKE :nome"); 
         $stmt->execute([':nome' => '%' . $input . '%']);
         $sites = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }else if ($type == 2) {
+    } else if ($type == 2) {
         $stmt = $pdo->prepare("SELECT * FROM dados WHERE email LIKE :nome"); 
         $stmt->execute([':nome' => '%' . $input . '%']);
         $sites = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }else {
+    } else {
         $stmt = $pdo->prepare("SELECT * FROM dados WHERE senha LIKE :nome"); 
         $stmt->execute([':nome' => '%' . $input . '%']);
         $sites = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -22,21 +22,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Procurador de Dados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    tabelas com tudo ae mn
+<body class="container mt-5">
+    <h2>Resultados da Busca</h2>
     <hr>
     <table class="table">
-        <?php foreach($sites as $c) { ?>
+        <thead>
             <tr>
-                <td>id: <?= $c['id_dados']?> </td>
-                <td>url: <?= $c['urls']?> </td>
-                <td>email: <?= $c['email']?> </td>
-                <td>senha: <?= $c['senha']?> </td>
+                <th>ID</th>
+                <th>URL</th>
+                <th>Email</th>
+                <th>Senha</th>
             </tr>
-        <?php } ?>
+        </thead>
+        <tbody>
+            <?php foreach($sites as $c) { ?>
+                <tr>
+                    <td><?= $c['id_dados']?></td>
+                    <td><?= $c['urls']?></td>
+                    <td><?= $c['email']?></td>
+                    <td><?= $c['senha']?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
     </table>
+    <!-- Include Bootstrap JavaScript and Popper.js (place these before the closing </body> tag) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-b4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyEOU8bHs3t9T5O8xWIrUZhQK1OeANth9y" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-pzjw8a+tpfWTZI6qPPTfiu6Ij0d8f6GltTUQd2I8Njds3+q0F2FflJ66cuutL3tx" crossorigin="anonymous"></script>
 </body>
 </html>
+                
--- index.php
+++ index.php
@@ -1,23 +1,31 @@
-<?php
-    require_once "connec.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>procurador de dados</title>
+    <title>Procurador de Dados</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <form action="coisas.php" method="POST">
-        <input type="text" name="nomesite">
-        <select name="tipopa">
-            <option value="1">URL</option>
-            <option value="2">EMAIL</option>
-            <option value="3">SENHA</option>
-        </select>
-        <input type="submit" value="obter">
+<body class="container mt-5">
+
+    <form action="coisas.php" method="POST" class="mb-3">
+        <div class="mb-3">
+            <label for="nomesite" class="form-label">Nome do Site</label>
+            <input type="text" name="nomesite" class="form-control" id="nomesite">
+        </div>
+        <div class="mb-3">
+            <label for="tipopa" class="form-label">Tipo de Dado</label>
+            <select name="tipopa" class="form-select" id="tipopa">
+                <option value="1">URL</option>
+                <option value="2">EMAIL</option>
+                <option value="3">SENHA</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Obter</button>
     </form>
-    
+
+    <!-- Include Bootstrap JavaScript and Popper.js (place these before the closing </body> tag) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-b4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyEOU8bHs3t9T5O8xWIrUZhQK1OeANth9y" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-pzjw8a+tpfWTZI6qPPTfiu6Ij0d8f6GltTUQd2I8Njds3+q0F2FflJ66cuutL3tx" crossorigin="anonymous"></script>
 </body>
 </html>
