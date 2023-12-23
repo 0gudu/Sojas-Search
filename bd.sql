65b2f480f2236c0cd1b957fa9cf8a831fb503ee6
0gudu <gugag3rr@gmail.com>
2023-12-23 05:21:12 -0300

    novo db

--- bd.sql
+++ bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 22, 2023 at 03:29 AM
+-- Generation Time: Dec 23, 2023 at 08:20 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -30,18 +30,48 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `dados`;
 CREATE TABLE IF NOT EXISTS `dados` (
   `id_dados` int NOT NULL AUTO_INCREMENT,
+  `fds` varchar(85) NOT NULL,
   `urls` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` longtext NOT NULL,
   `senha` longtext NOT NULL,
   PRIMARY KEY (`id_dados`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `dados`
 --
 
-INSERT INTO `dados` (`id_dados`, `urls`, `email`, `senha`) VALUES
-(1, 'siteexemplo.com', 'emailexemplo@ex.com', 'senhaex123');
+INSERT INTO `dados` (`id_dados`, `fds`, `urls`, `email`, `senha`) VALUES
+(1, '', 'siteexemplo.com', 'emailexemplo@ex.com', 'senhaex123'),
+(2, '', 'url1', 'email1', 'senha1'),
+(3, '', 'url2', 'email2', 'senha2'),
+(4, '', 'url3', 'email3', 'senha3'),
+(5, 'url1', 'email1', 'senha1', 'ui\r'),
+(6, 'url2', 'email2', 'senha2', 'ui\r'),
+(7, 'url3', 'email3', 'senha3', 'ui\r'),
+(8, 'url1', 'email1', 'senha1', 'ui\r'),
+(9, 'url2', 'email2', 'senha2', 'ui\r'),
+(10, 'url3', 'email3', 'senha3', 'ui\r');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `databasess`
+--
+
+DROP TABLE IF EXISTS `databasess`;
+CREATE TABLE IF NOT EXISTS `databasess` (
+  `id_database` int NOT NULL AUTO_INCREMENT,
+  `names` varchar(85) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id_database`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `databasess`
+--
+
+INSERT INTO `databasess` (`id_database`, `names`) VALUES
+(2, 'a');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
